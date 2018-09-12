763225813533c3b41cc6aa3e4e0d9b3e57fe12e4
spectralfox <spectralfox05@gmail.com>
2018-09-12 16:45:12 +0900

    디비연동 작업중
    일단 커밋

---
+++ itchat/src/main/java/com/yokipa/itchat/user/bd/service/CtgrSrchService.java
@@ -0,0 +1,16 @@
+package com.yokipa.itchat.user.bd.service;
+
+/**
+ * @Class : CtgrSrchService
+ * @Description : 카테고리검색서비스
+ * @Type: Service
+ * @Modification
+ * 
+ *  ������      			������              ��������
+ *  ---------   ---------   -------------------------------
+ *  2018.09.06		������		�űԵ��
+ *
+ */
+public interface CtgrSrchService {
+
+}
---
+++ itchat/src/main/java/com/yokipa/itchat/user/bd/service/impl/CtgrSrchServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yokipa.itchat.user.bd.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yokipa.itchat.admin.bd.service.CtgrSrchService;
+/**
+ * @Class : CtgrSrchServiceImpl
+ * @Description : 카테고리검색서비스구현
+ * @Type: ServiceImpl
+ * @Modification
+ * 
+ *   수정일      수정자              수정내용
+ *  ---------   ---------   -------------------------------
+ *  2018.09.08	                     신규
+ *
+ */
+@Service
+public class CtgrSrchServiceImpl implements CtgrSrchService {
+
+}
